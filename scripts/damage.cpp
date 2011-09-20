1126ddeaaf330598dd96171a69acc7c09f21b546
David C. Bishop <david@davidbishop.org>
2011-09-20 16:55:41 +1000

    Removed useless header file.

--- scripts/damage.cpp
+++ scripts/damage.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 using namespace std;
 
-#include "damage.hpp"
-
 #include "../src/TestClass.hpp"
 
 void initilize() {
--- scripts/damage.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef DAMAGE_HPP_
-#define DAMAGE_HPP_
-
-class TestClass;
-void initilize();
-void attack(TestClass* tc);
-
-#endif /* DAMAGE_HPP_ */
