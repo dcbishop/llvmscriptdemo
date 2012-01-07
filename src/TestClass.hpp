d387b62d557b1d8f3e2cb03fc6a0a9e4a2f32f53
David C. Bishop <david@davidbishop.org>
2012-01-08 10:25:20 +1100

    Added CC0 licensing.

--- src/TestClass.hpp
+++ src/TestClass.hpp
@@ -1,3 +1,16 @@
+/*
+ * TestClass.hpp - Example LLVM 'script'. Example native class to be accessed.
+ *
+ * Written in 2011 by David C. Bishop <david@davidbishop.org>
+ *
+ * To the extent possible under law, the author(s) have dedicated all copyright
+ * and related and neighboring rights to this software to the public domain
+ * worldwide. This software is distributed without any warranty.
+ *
+ * You should have received a copy of the CC0 Public Domain Dedication along
+ * with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+
 #ifndef TESTCLASS_HPP_
 #define TESTCLASS_HPP_
 
--- src/llvmscriptdemo.cpp
+++ src/llvmscriptdemo.cpp
@@ -1,3 +1,17 @@
+/*
+ * llvmscriptdemp.cpp - Example LLVM 'script'. Integrates LLVM bitcode with
+ * native C++.
+ *
+ * Written in 2011 by David C. Bishop <david@davidbishop.org>
+ *
+ * To the extent possible under law, the author(s) have dedicated all copyright
+ * and related and neighboring rights to this software to the public domain
+ * worldwide. This software is distributed without any warranty.
+ *
+ * You should have received a copy of the CC0 Public Domain Dedication along
+ * with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+
 #include <string>
 #include <memory>
 #include <iostream>
